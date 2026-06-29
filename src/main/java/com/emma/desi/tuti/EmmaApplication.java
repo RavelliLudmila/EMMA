b55785b093e81992ae6fb29e5ebe3676d8b83133
Ludmila <lud.ravelli@gmail.com>
2026-06-29 17:22:12 -0300

    Agregar clase principal EmmaApplication y pruebas de contexto para la aplicación

--- src/main/java/com/emma/desi/tuti/EmmaApplication.java
+++ src/main/java/com/emma/desi/tuti/EmmaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Emma1Application {
+public class EmmaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Emma1Application.class, args);
+		SpringApplication.run(EmmaApplication.class, args);
 	}
 
 }
